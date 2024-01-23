743acc9baf565eea24c5e2bff488b7262308f251
Deepak <dk376907@gmail.com>
2024-01-23 23:59:31 +0530

    completed TopBar

---
+++ src/header/Header.js
@@ -0,0 +1,15 @@
+import TopBarStyle from ".\topbar\TopBar"
+
+export default function Header(){
+    return(
+        <Header>
+            <div className="header">
+                header
+            </div>
+        </Header>
+    )
+}
+
+const Header = styled . div`
+
+`;
