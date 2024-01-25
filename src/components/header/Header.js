ed333fa644f70a1fae04828fa3872b3ba206621e
Deepak <dk376907@gmail.com>
2024-01-25 22:32:56 +0530

    updating

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,3 +1,5 @@
+import styled from "styled-components";
+
 export default function Header() {
   return (
     <Header1>
---
+++ src/components/post/Post.js
@@ -0,0 +1,104 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import "./post.css";
+
+export default function Post({ img }) {
+  return (
+    <Post1>
+      <div className="post">
+        <img className="postImg" src={img} alt="" />
+        <div className="postInfo">
+          <div className="postCats">
+            <span className="postCat">
+              <Link className="link" to="/posts?cat=Music">
+                Music
+              </Link>
+            </span>
+            <span className="postCat">
+              <Link className="link" to="/posts?cat=Music">
+                Life
+              </Link>
+            </span>
+          </div>
+          <span className="postTitle">
+            <Link to="/post/abc" className="link">
+              Lorem ipsum dolor sit amet
+            </Link>
+          </span>
+          <hr />
+          <span className="postDate">1 hour ago</span>
+        </div>
+        <p className="postDesc">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+          officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+          fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+          atque, exercitationem quibusdam, reiciendis odio laboriosam?
+        </p>
+      </div>
+    </Post1>
+  );
+}
+
+const Post1 = styled.div`
+  .post {
+    width: 385px;
+    margin: 0px 25px 40px 25px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .postImg {
+    width: 385px;
+    height: 280px;
+    object-fit: cover;
+    border-radius: 7px;
+  }
+
+  .postInfo {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .postCat {
+    font-family: "Varela Round", Arial, Helvetica, sans-serif;
+    font-weight: 400;
+    font-size: 11px;
+    color: #be9656;
+    line-height: 19px;
+    margin-top: 15px;
+    margin-right: 10px;
+    cursor: pointer;
+  }
+
+  .postTitle {
+    font-family: "Josefin Sans", Arial, Helvetica, sans-serif;
+    font-size: 24px;
+    font-weight: 900;
+    margin-top: 15px;
+    cursor: pointer;
+  }
+
+  .postDate {
+    font-family: "Lora", serif;
+    font-style: italic;
+    font-size: 13px;
+    font-weight: 400;
+    color: #999999;
+    margin-top: 15px;
+  }
+
+  .postDesc {
+    font-family: "Varela Round", Arial, Helvetica, sans-serif;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 24px;
+    color: #444444;
+    margin-top: 15px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 4;
+    -webkit-box-orient: vertical;
+  }
+`;
